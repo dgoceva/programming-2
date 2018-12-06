1fb03ae492992d6311c6a7c614f6af68163c875e
uktc-programming <44052125+uktc-programming@users.noreply.github.com>
2018-12-06 12:34:24 +0200

    Last sample-06-12-2018

---
+++ StudentProject/src/StudentProject/src/Student.java
@@ -0,0 +1,41 @@
+
+public class Student {
+	private short number;
+	private String name;
+	private float avMark;
+	
+	public short getNumber() {
+		return number;
+	}
+	public void setNumber(short number) {
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getAvMark() {
+		return avMark;
+	}
+	public void setAvMark(float avMark) {
+		this.avMark = avMark;
+	}
+//	@Override
+	public String toString() {
+		return "Student [number=" + number + ", name=" + name + ", avMark=" + avMark + "]";
+	}
+	public Student(short number, String name, float avMark) {
+		super();
+		this.number = number;
+		this.name = name;
+		this.avMark = avMark;
+	}
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
